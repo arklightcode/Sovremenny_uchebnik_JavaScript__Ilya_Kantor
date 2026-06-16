a5db6105730b062b4d32ca7433f282afc07ed101
Artur Belyak <arturbwc@gmail.com>
2026-06-16 17:37:12 +0300

    ch. 8

---
+++ Base JS/ch. 8/task 3/script.js
@@ -0,0 +1,14 @@
+"" + 1 + 0 // '10' str
+"" - 1 + 0 // -1 int 
+true + false // 1 int
+6 / "3" // 2 int
+"2" * "3" // 6 int
+4 + 5 + "px" // '9px' str
+"$" + 4 + 5 // '$45' str
+"4" - 2 // 2 int
+"4px" - 2  // NaN
+"  -9  " + 5 // '   -9   5' str
+"  -9  " - 5 //-14 int
+null + 1 // 1 int
+undefined + 1 // Nan
+" \t \n" - 2 // -2 
