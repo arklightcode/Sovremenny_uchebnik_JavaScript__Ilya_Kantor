1cab9bef682ff59ac5627ef3b1f250b3b9a7397a
Artur Belyak <arturbwc@gmail.com>
2026-06-18 17:09:00 +0300

    ch. 10

---
+++ Base JS/ch. 10/task 2/script.js
@@ -0,0 +1,7 @@
+let result = prompt('Какое официальное название Javascript?', '');
+
+if(result == 'ECMAScript') {
+    alert('Верно!')
+} else {
+    alert('Не знаете? ECMAScript!')
+}
---
+++ Base JS/ch. 10/task 3/script.js
@@ -0,0 +1,9 @@
+let res = prompt('Введите число:', '');
+
+if (res > 0) {
+  alert('1')
+} else if (res < 0) {
+  alert('-1')
+} else {
+  alert()
+}
